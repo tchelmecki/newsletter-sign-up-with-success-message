ce7bb5706467ef5bb4881cdbf99ccf296e470f82
Tomasz Chełmecki <tomaszchelmecki123@gmail.com>
2023-12-24 16:06:31 +0100

    Init project

---
+++ script.js
@@ -0,0 +1,12 @@
+const email = document.querySelector('input');
+const emailCheck = document.querySelector('.email-check');
+
+email.addEventListener("input",(e)=>{
+    try {
+        
+    } catch (error) {
+        
+    }
+    // // emailCheck.textContent = e.target.value;
+    // console.log(e);
+})
