bce55276160d2e8ff0236dda399c29eeb976d3fd
Tomasz Chełmecki <tomaszchelmecki123@gmail.com>
2023-12-24 23:28:14 +0100

    Update script.js (handle error state)

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 const email = document.querySelector('input');
 const emailCheck = document.querySelector('.email-check');
+const button = document.querySelector('button');
 
-email.addEventListener("input",(e)=>{
-    try {
-        
-    } catch (error) {
-        
+function validateEmail() {
+    var mailFormat =  /\S+@\S+\.\S+/;
+    if (email.value.match(mailFormat)) {
+        emailCheck.classList.remove('active');
+        email.classList.remove('invalid');
+    return true;
+    } else {
+      emailCheck.classList.add('active');
+      email.classList.add('invalid');
+      return false;
     }
-    // // emailCheck.textContent = e.target.value;
-    // console.log(e);
-})
+}
+
+button.addEventListener('click', validateEmail);
+  
