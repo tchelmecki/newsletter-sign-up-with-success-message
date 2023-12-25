6ad6b065ba4db35a25dc26e2783acd58b2f20de2
Tomasz Chełmecki <tomaszchelmecki123@gmail.com>
2023-12-25 12:06:45 +0100

    Update an error state and create a success state

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
-const email = document.querySelector('input');
+const emailInput = document.querySelector('input');
 const emailCheck = document.querySelector('.email-check');
 const button = document.querySelector('button');
+const successMessage = document.querySelector('.success-message');
+const main = document.querySelector('main');
+const body = document.querySelector('body');
+
+function addElement(){
+  const successDiv = document.createElement("div");
+  successDiv.className = 'success-message';
+  const email = emailInput.value;
+  successDiv.innerHTML = `
+  <div class="success-message">
+  <img src="assets/images/icon-success.svg">
+  <p>Thanks for subscribing!</p>
+  <p>A confirmation email has been sent to ${email}.com. 
+    Please open it and click the button inside to confirm your subscription.</p>
+  <button>Dismiss message</button>
+  </div>`;
+  body.appendChild(successDiv);
+
+}
 
 function validateEmail() {
     var mailFormat =  /\S+@\S+\.\S+/;
-    if (email.value.match(mailFormat)) {
+    if (emailInput.value.match(mailFormat)) {
         emailCheck.classList.remove('active');
-        email.classList.remove('invalid');
-    return true;
+        emailInput.classList.remove('invalid');
+        main.style.display = "none";
+        addElement();
+        // successMessage.style.display = "flex";
+      //  return true;
     } else {
       emailCheck.classList.add('active');
-      email.classList.add('invalid');
-      return false;
+      emailInput.classList.add('invalid');
+      // return false;
     }
 }
 
